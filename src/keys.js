e731f134660d29054da1784cb04be3cc05d06397
GUADALUPE MONSERRAT OROPEZA SANCHEZ <monse.oropeza@comunidad.unam.mx>
2025-11-06 17:00:15 -0600

    Primer commit: Estructura inicial del proyecto - vista de carrito empezada

---
+++ src/keys.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+    database: {
+        host: '192.168.80.152',
+        user: 'root',
+        port: '30306',
+        password: 'Mailo281001',
+        database:'tienda_peliculas'
+    }
+};
---
+++ src/routes/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req,res) =>{
+    res.send("Heloo World");
+});
+
+module.exports = router;
